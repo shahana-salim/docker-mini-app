f662552c03b22438f523e49b9c82b602c3d7205f
shahana <shahana8281@gmail.com>
2025-11-07 23:13:22 +0530

    update frontend to use render backend url

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ function App() {
   const [message, setMessage] = useState('');
 
   useEffect(() => {
-    fetch('http://localhost:5000/api/hello')
+    fetch('https://docker-mini-backend-5sem.onrender.com/api/hello')
       .then(res => res.json())
       .then(data => setMessage(data.message));
   }, []);
